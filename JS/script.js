d202b29a5319cec5abd1337fc4b40c82958918ce
Lucas-Feat <lucas.feat.lf@gmail.com>
2022-04-05 12:04:36 +0200

    carousel ok

--- JS/script.js
+++ JS/script.js
@@ -21,8 +21,12 @@ document.getElementById("scrollToTop").addEventListener("click", () => {
 });
 document.getElementById("scrollToTop").style.cursor = "pointer";
 
+/*-----------SCROLLPROGRESS-------------*/
+
 let scrollPercentage = () => {
   let scrollProgress = document.getElementById("scrollToTop");
+  let scrollWhite = document.getElementById("scrollProgress");
+  let scrollImg = document.getElementById("scrollImg");
   let pos = document.documentElement.scrollTop;
   let calcHeight =
     document.documentElement.scrollHeight -
@@ -31,5 +35,19 @@ let scrollPercentage = () => {
   let scrollValue = Math.round((pos * 100) / calcHeight);
 
   scrollProgress.style.background = `conic-gradient(#e93119 ${scrollValue}%,rgba(0,0,0,0) ${scrollValue}%)`;
+
+  if (scrollValue > 20) {
+    scrollImg.style.opacity = 1;
+  } else {
+    scrollImg.style.opacity = 0;
+  }
+
+  if (scrollValue > 25) {
+    scrollProgress.style.opacity = 1;
+    scrollWhite.style.opacity = 1;
+  } else {
+    scrollProgress.style.opacity = 0;
+    scrollWhite.style.opacity = 0;
+  }
 };
 window.onscroll = scrollPercentage;
