15c323817e954dcd825c03b1d373b6a24f9c9c53
Lucas-Feat <lucas.feat.lf@gmail.com>
2022-04-07 16:11:00 +0200

    contact responsive ajout menu à faire

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -1,26 +1,26 @@
-let hidebar = () => {
-  let pos = document.documentElement.scrollTop;
-  if (pos < 200) {
-    document.getElementById("header").style.transform = "translateY(-200px)";
-  } else {
-    document.getElementById("header").style.transform = "translateY(0)";
-  }
-};
+let isMouseHover = false;
 
 let shownav = () => {
-  console.log("in");
   document.getElementById("header").style.transform = "translateY(0)";
 };
 let hidenav = () => {
-  console.log("out");
   document.getElementById("header").style.transform = "translateY(-200px)";
 };
 
 document.getElementById("logo").addEventListener("mouseover", () => {
-  console.log("in");
   shownav();
 });
 document.getElementById("header").addEventListener("mouseleave", () => {
-  console.log("out");
+  isMouseHover = false;
   hidenav();
+});
+
+document.getElementById("header").addEventListener("mouseover", () => {
+  isMouseHover = true;
+});
+
+document.getElementById("logo").addEventListener("mouseleave", () => {
+  if (isMouseHover) {
+    hidenav();
+  }
 });
